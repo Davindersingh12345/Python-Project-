45a69e7b127d5c279cecbee684d62807c29ff958
Davinder Singh <06davindersingh@gmail.com>
2025-04-09 01:01:16 +0530

    Read Data

---
+++ data.py
@@ -0,0 +1,6 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+data1 = pd.read_csv(r"C:\Users\ASUS\Downloads\Air_Quality.csv") #read csv file 
+df = pd.DataFrame(data1) 
+print(df)
